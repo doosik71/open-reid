ae20f61d5b11f5f86944c18c50de4aa492408ca0
Doosik Kim <doosik@etri.re.kr>
2020-10-21 18:16:18 +0900

    Update requirements.

--- install.bat
+++ install.bat
@@ -1,2 +1,1 @@
 pip install -r requirements.txt
-pip install torch===1.2.0 torchvision===0.4.0 -f https://download.pytorch.org/whl/torch_stable.html
--- runme.bat
+++ runme.bat
@@ -1,6 +1,4 @@
-rem python examples/softmax_loss.py -d viper      -b 64 -j 2 -a resnet50                      --logs-dir logs/softmax-loss/viper-resnet50
-rem python examples/softmax_loss.py -d market1501 -b 64 -j 2 -a resnet50                      --logs-dir logs/softmax-loss/market1501-resnet50-euclidean
-    python examples/softmax_loss.py -d market1501 -b 64 -j 2 -a resnet50 --dist-metric kissme --logs-dir logs/softmax-loss/market1501-resnet50-kissme
-
-
-
+rem python examples/softmax_loss.py -d market1501 -b 64 -j 2 -a resnet50                      --logs-dir logs/softmax-loss/market1501-resnet50-euclidean-50
+    python examples/softmax_loss.py -d market1501 -b 64 -j 2 -a resnet50                      --logs-dir logs/softmax-loss/market1501-resnet50-euclidean-500  --epochs 500
+rem python examples/softmax_loss.py -d market1501 -b 64 -j 2 -a resnet50 --dist-metric kissme --logs-dir logs/softmax-loss/market1501-resnet50-kissme-50
+rem python examples/softmax_loss.py -d market1501 -b 64 -j 2 -a resnet50 --dist-metric kissme --logs-dir logs/softmax-loss/market1501-resnet50-kissme-500     --epochs 500
